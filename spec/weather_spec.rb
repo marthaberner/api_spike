f9041de8bf37c89651db8fdc1b678874181bcbd5
marthaberner <marthaberner.mb@gmail.com>
2014-05-05 17:26:00 -0600

    Displays 7 day forecast

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -5,6 +5,12 @@ describe Weather do
   it 'Displays the weather for a city' do
     @weather = Weather.new('denver,co')
 
-    expect(@weather.forecast).to eq 'Sky is Clear'
+    expect(@weather.forecast).to eq 'few clouds'
+  end
+
+  it 'Displays a 7 day forecast' do
+    @weather = Weather.new('chicago,il')
+
+    expect(@weather.seven_day).to eq 'Clouds, Clouds, Clouds, Rain, Rain, Clear, Rain'
   end
 end
--- weather.rb
+++ weather.rb
@@ -7,9 +7,18 @@ class Weather
   end
 
   def forecast
-    weather = Faraday.get('http://api.openweathermap.org/data/2.5/weather?q=#{@city')
+    weather = Faraday.get("http://api.openweathermap.org/data/2.5/weather?q=#{@city}")
     json_string = JSON.parse(weather.body)
     json_string['weather'].first['description']
+  end
+
+  def seven_day
+    outlook = Faraday.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=#{@city}&mode=json&cnt=7")
+    json_output = JSON.parse(outlook.body)
+    return_value = json_output['list'].map do |day|
+    day['weather'].first['main']
+    end
+    return_value.join(", ")
   end
 end
 
